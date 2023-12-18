7e8223b69b86a27b2d95e2f87ac0ee00dd2b5542
mbledkowski <pub@mble.dk>
2023-12-18 22:13:45 +0100

    Add edge support to server - router

--- interface/src/server/api/routers/graph.ts
+++ interface/src/server/api/routers/graph.ts
@@ -9,26 +9,7 @@ await db.build();
 
 export const graphRouter = createTRPCRouter({
   nodes: publicProcedure
-    .query(async () => { const nodes = await db.getNodes(); return nodes; })
-  // hello: publicProcedure
-  //   .input(z.object({ text: z.string() }))
-  //   .query(({ input }) => {
-  //     return {
-  //       greeting: `Hello ${input.text}`,
-  //     };
-  //   }),
-
-  // create: publicProcedure
-  //   .input(z.object({ name: z.string().min(1) }))
-  //   .mutation(async ({ input }) => {
-  //     // simulate a slow db call
-  //     await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  //     post = { id: post.id + 1, name: input.name };
-  //     return post;
-  //   }),
-
-  // getLatest: publicProcedure.query(() => {
-  //   return post;
-  // }),
+    .query(async () => { const nodes = await db.getNodes(); return nodes; }),
+  edges: publicProcedure
+    .query(async () => { const nodes = await db.getEdges(); return nodes; })
 });
